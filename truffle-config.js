86991a36cea4d482023a31ad1f87b2bf8bafb888
JanetMo <m.marz@kabelmail.de>
2022-07-24 11:09:25 +0200

    changes

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,9 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+ const HDWalletProvider = require("@truffle/hdwallet-provider");
+ const dotenv = require("dotenv");
+ dotenv.config();
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -35,6 +37,26 @@ module.exports = {
    */
 
   networks: {
+
+       ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 500, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+  
+
     xdai: {
       provider: function() {
             return new HDWalletProvider(
@@ -42,14 +64,49 @@ module.exports = {
            "https://rpc.gnosischain.com")
       },
       network_id: 100,
+      timeoutBlocks: 500000,
       gas: 500000,
       gasPrice: 1000000000
-},
-development: {
+    },
+      development: {
+        host: "127.0.0.1",
+        port: 8545,
+        network_id: "*" // Match any network id
+      },
+
+    poa: {
+      provider: function() {
+            return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://sokol.poa.network")
+      },
+      network_id: 77,
+      timeoutBlocks: 500000,
+      gas: 500000,
+      gasPrice: 1000000000
+    },
+    development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-}
+    },
+
+    matic: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://matic-mumbai.chainstacklabs.com/`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 500,
+      skipDryRun: true
+    },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+  },
+
+
+
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache, geth, or parity) in a separate terminal
@@ -74,14 +131,15 @@ development: {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // ropsten: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    //   network_id: 3,       // Ropsten's id
-    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    //ropsten: {
+    //provider: () => new HDWalletProvider(process.env.MNEMONIC,
+    //  `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`);
+    //network_id: 3,       // Ropsten's id
+    //gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    //confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+    //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    //},
     //
     // Useful for private networks
     // private: {
@@ -89,11 +147,11 @@ development: {
     //   network_id: 2111,   // This network is yours, in the cloud.
     //   production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
+  
 
   // Set default mocha options here, use special reporters, etc.
   mocha: {
-    // timeout: 100000
+    timeout: 500000
   },
 
   // Configure your compilers
